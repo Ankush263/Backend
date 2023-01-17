b95149c9e7e542dc94b1bfd52677ae2dc24de80c
Ankush263 <ankushbanik263@gmail.com>
2023-01-17 20:15:38 +0530

    update

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require("express")
 const app = express()
 
 const farmRouter = require("./routes/farmRoutes.js")
-// const meatRoute = require("./routes/meatRoutes.js")
-// const produceRoute = require("./routes/produceRoute.js")
 const AllMeatRoutes = require("./routes/meatRoutes/AllMeatRoutes.js")
 
 app.use(express.json())
@@ -14,8 +12,6 @@ app.use((req, res, next) => {
 })
 
 app.use("/api/v1/farm", farmRouter)
-// app.use("/api/v1/farm/meat", meatRoute)
-// app.use("/api/v1/farm/produce", produceRoute)
 app.use("/api/v1/farm/meat/allmeat", AllMeatRoutes)
 
 module.exports = app
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const app = require("./app")
+const fs = require("fs")
+const Farm = require("./model/farmModel")
+const sampleFarm = require("./data/sample_farm_data.json")
 
 dotenv.config({ path: "./.env" })
 const DB = process.env.DATABASE
@@ -12,6 +15,19 @@ mongoose.connect(DB, {
 }).then((con) => {
   console.log("DB connected successfully")
 })
+
+const farms = JSON.parse(fs.readFileSync(`${__dirname}/data/sample_farm_data.json`, "utf-8"))
+
+const importData = async () => {
+  try {
+    await Farm.create(farms)
+    console.log("Data successfully loaded....")
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// importData()
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
