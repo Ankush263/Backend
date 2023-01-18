0080e4467c7f90e25f5ebc36bb3ee670b11263f4
Ankush263 <ankushbanik263@gmail.com>
2023-01-18 20:55:15 +0530

    update berry model

--- model/produce/berryModel.js
+++ model/produce/berryModel.js
@@ -1,100 +1,101 @@
 const mongoose = require("mongoose")
 const Farm = require("../farmModel.js")
 
+const detailsSchema = new mongoose.Schema({
+  details: [
+    {
+      packsWeight: String,
+      price: Number,
+      img: String
+    }
+  ]
+})
+
 const berrySchema = new mongoose.Schema({
-  subDoc: {
+  farm_Id: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Farm'
   },
   blueBerry: {
-    available: Boolean,
-    details: [
-      {
-        packsWeight: String,
-        price: Number,
-        img: String
-      }
-    ]
+    type: Boolean
+  },
+  blueBerryDetails: {
+    type: detailsSchema,
+    validate: function() {
+      return this.blueBerry
+    }
   },
   strawBerry: {
-    available: Boolean,
-    details: [
-      {
-        packsWeight: String,
-        price: Number,
-        img: String
-      }
-    ]
+    type: Boolean
+  },
+  strawBerryDetails: {
+    type: detailsSchema,
+    validate: function() {
+      return this.strawBerry
+    }
   },
   cranBerry: {
-    available: Boolean,
-    details: [
-      {
-        packsWeight: String,
-        price: Number,
-        img: String
-      }
-    ]
+    type: Boolean
+  },
+  cranBerryDetails: {
+    type: detailsSchema,
+    validate: function() {
+      return this.cranBerry
+    }
   },
   blackBerry: {
-    available: Boolean,
-    details: [
-      {
-        packsWeight: String,
-        price: Number,
-        img: String
-      }
-    ]
+    type: Boolean
+  },
+  blackBerryDetails: {
+    type: detailsSchema,
+    validate: function() {
+      return this.blackBerry
+    }
   },
   blackCurrant: {
-    available: Boolean,
-    details: [
-      {
-        packsWeight: String,
-        price: Number,
-        img: String
-      }
-    ]
+    type: Boolean
+  },
+  blackCurrantDetails: {
+    type: detailsSchema,
+    validate: function() {
+      return this.blackCurrant
+    }
   },
   salmonBerry: {
-    available: Boolean,
-    details: [
-      {
-        packsWeight: String,
-        price: Number,
-        img: String
-      }
-    ]
+    type: Boolean
+  },
+  salmonBerryDetails: {
+    type: detailsSchema,
+    validate: function() {
+      return this.salmonBerry
+    }
   },
   gooseBerry: {
-    available: Boolean,
-    details: [
-      {
-        packsWeight: String,
-        price: Number,
-        img: String
-      }
-    ]
+    type: Boolean
+  },
+  gooseBerryDetails: {
+    type: detailsSchema,
+    validate: function() {
+      return this.gooseBerry
+    }
   },
   mulBerry: {
-    available: Boolean,
-    details: [
-      {
-        packsWeight: String,
-        price: Number,
-        img: String
-      }
-    ]
+    type: Boolean
+  },
+  mulBerryDetails: {
+    type: detailsSchema,
+    validate: function() {
+      return this.mulBerry
+    }
   },
   lingonBerry: {
-    available: Boolean,
-    details: [
-      {
-        packsWeight: String,
-        price: Number,
-        img: String
-      }
-    ]
+    type: Boolean
+  },
+  lingonBerryDetails: {
+    type: detailsSchema,
+    validate: function() {
+      return this.lingonBerry
+    }
   }
 })
 
