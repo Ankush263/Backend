5278ef627dfb148153091dd732699428fb6983d7
Ankush263 <ankushbanik263@gmail.com>
2023-01-19 11:28:36 +0530

    update

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express")
 const app = express()
 
 const farmRouter = require("./routes/farmRoutes.js")
-const AllMeatRoutes = require("./routes/meatRoutes/AllMeatRoutes.js")
+const meatRouter = require("./routes/meatRouter.js")
+const produceRouter = require("./routes/produceRouter.js")
 
 app.use(express.json())
 
@@ -12,6 +13,8 @@ app.use((req, res, next) => {
 })
 
 app.use("/api/v1/farm", farmRouter)
-app.use("/api/v1/farm/meat/allmeat", AllMeatRoutes)
+
+app.use("/api/v1/meat", meatRouter)
+app.use("/api/v1/produce", produceRouter)
 
 module.exports = app
