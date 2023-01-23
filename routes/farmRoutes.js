49d438285911e75ae91a8be54e454623789df017
Ankush263 <ankushbanik263@gmail.com>
2023-01-23 22:41:41 +0530

    update

--- routes/farmRoutes.js
+++ routes/farmRoutes.js
@@ -16,10 +16,14 @@ const router = express.Router()
 
 router.route("/farm-stats").get(getFarmStats)
 
+// router
+//   .route("/")
+//     .get(protect, getAllFarms)
+//     .post(protect, createFarm)
 router
   .route("/")
-    .get(protect, getAllFarms)
-    .post(protect, createFarm)
+    .get(getAllFarms)
+    .post(createFarm)
 
 
 router
