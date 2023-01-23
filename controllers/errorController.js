0c5d6a2af7b90bcd2befc27e8814b04d6d3e86aa
Ankush263 <ankushbanik263@gmail.com>
2023-01-23 22:41:18 +0530

    update

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -18,6 +18,10 @@ const handleValidationError = err => {
   return new AppError(message, 400)
 }
 
+const handleJWTError = err => new AppError("Invalid Token, Please log in again", 401)
+
+const handleJWTExpiredError = err => new AppError("Your token got expired, please login again", 401)
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -34,6 +38,7 @@ const sendErrorProd = (err, res) => {
       message: err.message,
     })
   } else {
+    console.log(err)
     res.status(500).json({
       status: "error",
       message: "Something went very wrong"
@@ -53,6 +58,8 @@ module.exports = (err, req, res, next) => {
     if(error.name === "CastError") error = handleCastErrorDB(error)
     if(error.code === 11000) error = handleDuplicateFieldsDB(error)
     if(error.name === "ValidationError") error = handleValidationError(error)
+    if(error.name === "JsonWebTokenError") error = handleJWTError(error)
+    if(error.name === "TokenExpiredError") error = handleJWTExpiredError(error)
     sendErrorProd(error, res)
   }
   
